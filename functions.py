2dd6d07f74ba3cd3172adc5c5464ca7aa23b9e6e
Andrew <72750362+andrewc-moj@users.noreply.github.com>
2022-11-18 16:25:20 +0000

    Added a section for running on a mac and moved function def for findPageLinks

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+from constants import MAC_CHROME_DRIVER
 
 def initbrowser(url=None, hidebrowser=False):
 
@@ -37,11 +38,14 @@ def initbrowser(url=None, hidebrowser=False):
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {
-        #"download.default_directory": r"C:/Users/andre/Desktop", #Change default directory for downloads
+        #"download.default_directory": r"C:/Users/name/Desktop", #Change default directory for downloads
         "download.prompt_for_download": False, #To auto download the file
         "download.directory_upgrade": True,
         "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
         })
+
+    # What platform are we using?
+    print(f"Running on {sys.platform}")
 
     ###############################################
     ## Linux 
@@ -62,14 +66,27 @@ def initbrowser(url=None, hidebrowser=False):
         display.start()
         
         browser = webdriver.Chrome(driver_path, chrome_options=options)
-        
     
+    ###############################################
+    ## Mac 
+    ###############################################
+       
+    elif sys.platform == 'darwin':
+
+        # What platf
+        
+        # Path for Selenium driver 
+        browser_path = MAC_CHROME_DRIVER
+        print(f"Open browser with URL '{url}'")
+        browser = webdriver.Chrome(MAC_CHROME_DRIVER, chrome_options=options)
+
     ## Else, on windows - specify path
     else:
         #browser_path = "C:\\Program Files\\Python36\\selenium\\webdriver\\chrome\\chromedriver.exe"
         browser_path = os.environ['CHROME_DRIVER']
         sys.path.append(os.environ['CHROME_DRIVER'])
-        print("Opening browser with URL", url)
+        print(f"Open browser with URL '{url}'")
+
         browser = webdriver.Chrome(os.environ['CHROME_DRIVER'], chrome_options=options)
         
     
@@ -84,3 +101,25 @@ def initbrowser(url=None, hidebrowser=False):
     if url != None:
         browser.get(url)
     return browser
+
+def findPageLinks(browser):
+
+        # Go to bottom of page
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        time.sleep(2)
+        while True:
+            browser.execute_script("window.scrollTo(0, window.scrollY - 300)")
+            # Scroll up until you find pages
+            pages = browser.find_elements_by_xpath("//a[contains(@href, 'read-test')]")
+
+            # Get hrefs (urls)
+            pages = [p.get_property('href') for p in pages if not re.search('Read|^$', p.text, re.I)]
+
+            # Reverse list
+            #pages = pages[::-1]
+            numpages = len(pages)
+            if numpages > 0:
+                return pages
+
+        
